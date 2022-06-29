8fa6a99fc7fbe7e3d5033f828d990ae26219070c
moca99 <104081194+moca99@users.noreply.github.com>
2022-06-29 17:09:10 +0900

    Add files via upload

---
+++ js/content.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+      
+    //달력
+    $("#date1").datepicker({
+        showOn: "button",
+        buttonImage: "images/datepicker-icon.png",
+        buttonImageOnly: false,
+        buttonText: "Select date"
+    });
+
+    $("#date2").datepicker({
+        showOn: "button",
+        buttonImage: "images/datepicker-icon.png",
+        buttonImageOnly: false,
+        buttonText: "Select date"
+    });
+  
+   
+
+
+  
+});
+
+
